c66b35bd205af4057267c3d050068734d2a35c93
spacebuckett <shehxadv2.0@gmail.com>
2025-08-09 22:38:51 +0500

    Two Sum

---
+++ arrays/arrays-easy/1-two-sum.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+}
+
+// BRUTE FORCE METHOD - O(n2)
+//func twoSum(nums []int, target int) []int {
+//	for i := 0; i < len(nums); i++ {
+//		for j := i + 1; j < len(nums); j++ {
+//			if nums[i]+nums[j] == target {
+//				return []int{i, j}
+//			}
+//		}
+//	}
+//	return []int{0, 0}
+//}
+
+// OPTIMAL METHOD O(N)
+func twoSum(nums []int, target int) []int {
+	mapData := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		currentValue := nums[i]
+		moreNeeded := target - currentValue
+		_, exists := mapData[moreNeeded]
+		if exists {
+			return []int{mapData[moreNeeded], i}
+		} else {
+			mapData[currentValue] = i
+		}
+	}
+	return []int{0, 0}
+}
